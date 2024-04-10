8e2f55d7e9dde597d25601e78eab638fc609782b
Hajali-Selim <selimhajali@gmail.com>
2024-04-10 16:28:36 +0200

    general update

--- datasets/models.py
+++ datasets/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Dataset(models.Model):
     name, field = models.CharField(max_length=255), models.CharField('Discipline', max_length=255)
     space, time = models.CharField('Spatial scale', max_length=255, null=True), models.CharField('Temporal scale', max_length=255, null=True)
-    icon = models.ImageField('Icon', blank=True)
+    icon = models.ImageField('Icon', blank=True, upload_to='datasets/static/images/')
     sc1, sc2, sc3 = models.ImageField('Relational SC', blank=True, upload_to='datasets/static/images/'), models.ImageField('Layout SC', blank=True, upload_to='datasets/static/images/'), models.ImageField('Pragmatic SC', blank=True, upload_to='datasets/static/images/')
     fc1, fc2, fc3, fc4 = models.ImageField('Quantitative FC', blank=True, upload_to='datasets/static/images/'), models.ImageField('Qualitative FC', blank=True, upload_to='datasets/static/images/'), models.ImageField('Mechanistic FC', blank=True, upload_to='datasets/static/images/'), models.ImageField('Propagation FC', blank=True, upload_to='datasets/static/images/')
     slug = models.SlugField(default="", null=False)

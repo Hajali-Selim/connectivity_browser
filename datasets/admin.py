49fc6099fb55927a8b528ba791c6333c0b5776ba
Hajali-Selim <selimhajali@gmail.com>
2024-04-10 23:02:55 +0200

    update admin

--- datasets/admin.py
+++ datasets/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Dataset
 # Register your models here.
 
-@admin.register(Dataset)
-class DatasetAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-    prepopulated_fields = {"slug": ("name", "icon", "field", "space", "time", "sc1", "sc2", "sc3", "fc1", "fc2", "fc3", "fc4", )}
+admin.site.register(Dataset)
+#@admin.register(Dataset)
+#class DatasetAdmin(admin.ModelAdmin):
+#    readonly_fields = ('id',)
+#    prepopulated_fields = {"slug": ("name", "icon", "field", "space", "time", "sc1", "sc2", "sc3", "fc1", "fc2", "fc3", "fc4", )}
 

bbb8e27f4e8a6504da460c9960ca22969403d560
Hajali-Selim <selimhajali@gmail.com>
2024-04-10 16:57:21 +0200

    in production

--- conn_browser/settings.py
+++ conn_browser/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-deployment = False
+deployment = True
 
 if deployment:
     SECRET_KEY = os.environ.get("SECRET_KEY")

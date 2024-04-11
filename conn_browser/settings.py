1e244277ab1bede6f2a95281551fe307dfbfb972
Hajali-Selim <selimhajali@gmail.com>
2024-04-11 17:35:49 +0200

    debug propagate exceptions

--- conn_browser/settings.py
+++ conn_browser/settings.py
@@ -37,6 +37,7 @@ else:
     DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3",
                              "NAME": BASE_DIR / "db.sqlite3", }}
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
 STATIC_ROOT = "staticfiles"#os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 

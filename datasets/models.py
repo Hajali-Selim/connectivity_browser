36a1ed723c671b589a79349ab844f663473229c6
Hajali-Selim <selimhajali@gmail.com>
2024-04-12 16:53:19 +0200

    changing models upload_to folder (null=True, instead of blank=True for all imagefield)

--- datasets/models.py
+++ datasets/models.py
@@ -7,8 +7,8 @@ class Dataset(models.Model):
     name, field = models.CharField(max_length=255), models.CharField('Discipline', max_length=255)
     space, time = models.CharField('Spatial scale', max_length=255, null=True), models.CharField('Temporal scale', max_length=255, null=True)
     icon = models.ImageField('Icon', blank=True, upload_to=image_folder)
-    sc1, sc2, sc3 = models.ImageField('Relational SC', blank=True, upload_to=image_folder), models.ImageField('Layout SC', blank=True, upload_to=image_folder), models.ImageField('Pragmatic SC', blank=True, upload_to=image_folder)
-    fc1, fc2, fc3, fc4 = models.ImageField('Quantitative FC', blank=True, upload_to=image_folder), models.ImageField('Qualitative FC', blank=True, upload_to=image_folder), models.ImageField('Mechanistic FC', blank=True, upload_to=image_folder), models.ImageField('Propagation FC', blank=True, upload_to=image_folder)
+    sc1, sc2, sc3 = models.ImageField('Relational SC', null=True, upload_to=image_folder), models.ImageField('Layout SC', null=True, upload_to=image_folder), models.ImageField('Pragmatic SC', null=True, upload_to=image_folder)
+    fc1, fc2, fc3, fc4 = models.ImageField('Quantitative FC', null=True, upload_to=image_folder), models.ImageField('Qualitative FC', null=True, upload_to=image_folder), models.ImageField('Mechanistic FC', null=True, upload_to=image_folder), models.ImageField('Propagation FC', null=True, upload_to=image_folder)
     slug = models.SlugField(default="", null=False)
     def __str__(self):
         return f'{self.name}'
